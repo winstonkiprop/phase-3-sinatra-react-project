9f571bac59010dfba21d21f1bf2cd988648a30fc
winston kiprop <winstonekip50@gmail.com>
2022-09-21 23:50:06 +0300

    create seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,73 @@
 puts "🌱 Seeding spices..."
 
 # Seed your database here
+categories = Category.create([
+    {
+        category_name: "shopping",
+        description: "All household electronic items"
+    },
+    {
+        category_name: "entertainment",
+        description: "expenditure for entertainment"
+    },
+    {
+        category_name: "medical",
+        description: "expenditure for medication"
+    },
+    {
+        category_name: "food",
+        description: "expendituer for food"
+    }
+])
+
+items = Item.create ([
+    {
+     item_name: "sony televison",
+     price: 10000,
+     quantity: 0
+ },
+ {
+     item_name: "fridge",
+     price: 40000,
+     quantity: 0
+ },
+ {
+     item_name: "microwave",
+     price: 25000,
+     quantity: 0
+ },
+    {
+        item_name: "Netflix subscription",
+        price: 2000,
+        quantity: 0
+    },
+    {
+        item_name: "dental checkup",
+        price: 5000,
+        quantity: 0
+    },
+    {
+    item_name: "grocery",
+    price: 10000,
+    quantity: 1
+}
+ ])
+customers = Customer.create([
+    {
+        username: "Winstonne"
+    },
+    {
+        username: "Mabel"
+    },
+    {
+        username: "Maryanne"
+    }
+])
+
+expenses = Expense.create([
+    {
+        amount: 100
+    }
+])
 
 puts "✅ Done seeding!"
