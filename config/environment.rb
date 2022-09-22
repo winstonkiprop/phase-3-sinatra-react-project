1327f2d0283b8fc8f748a75f08f8ad8c122a2f6a
winston kiprop <winstonekip50@gmail.com>
2022-09-22 19:06:12 +0300

    server

--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,5 @@
+
+
 # This is an _environment variable_ that is used by some of the Rake tasks to determine
 # if our application is running locally in development, in a test environment, or in production
 ENV['RACK_ENV'] ||= "development"
@@ -5,6 +7,7 @@ ENV['RACK_ENV'] ||= "development"
 # Require in Gems
 require 'bundler/setup'
 Bundler.require(:default, ENV['RACK_ENV'])
+
 
 # Require in all files in 'app' directory
 require_all 'app'
