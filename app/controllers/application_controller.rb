67873f1f16aff0b27a0be7cb79ca7f48a20dfc47
winston kiprop <winstonekip50@gmail.com>
2022-09-22 12:19:50 +0300

    create endpoints

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,33 @@ class ApplicationController < Sinatra::Base
   get "/" do
     { message: "Good luck with your project!" }.to_json
   end
+get "/categories" do
+  categories = Category.all
+  categories.to_json
+end
 
-  
+get "/categories/:category_name" do
+  category = Category.find_by(params[:name])
+  category.to_json
+end
+
+post "/expenses" do
+  expense = Expense.create(
+    amount: params[:amount],
+    customer_id: params[:customer_id],
+    category_id: params[:category_id]
+  )
+  expense.to_json
+end
+
+get "/expenses" do
+  expenses = Expense.all
+  expenses.to_json
+end
+
+delete "/expenses/:id" do
+  expense = Expense.find(params[:id])
+  expense.destroy
+  expense.to_json
+end
 end
